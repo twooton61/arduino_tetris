a5a6b89a79c74248f1d39f158edfc1b79fa1d92c
Terry Wooton <terry@oodle.io>
2020-11-25 20:18:18 -0500

    LCD matrix works

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,35 @@
 /*
-   Server Robot
-   Author: twooton
-   Email: tcwooton@gmail.com
-   Description: Simple Robot
+  Arduino Tetris
+  Author: twooton
+  Email: tcwooton@gmail.com
+  Description: Simple Robot
 */
 
 #include <Arduino.h>
 #include <Helpers.h>
+#include <MaxMatrix.h>
+
+const int MAX_MATRICIES_ON_BOARD = 1;
+const int MAX_ROWS = 8;
+const int MAX_COLS = 8;
+const int MAX_MATRIX_DIN_PIN = DIGITAL_IO_PIN(11);
+const int MAX_MATRIX_CS_LOAD_PIN = DIGITAL_IO_PIN(10);
+const int MAX_MATRIX_CLK_PIN = DIGITAL_IO_PIN(13);
+
+MaxMatrix max_matrix(MAX_MATRIX_DIN_PIN, MAX_MATRIX_CS_LOAD_PIN, MAX_MATRIX_CLK_PIN, MAX_MATRICIES_ON_BOARD);
 
 void setup()
 {
   Serial.begin(9600);
+  max_matrix.init();
+  max_matrix.setIntensity(0);
 }
 
 void loop()
 {
   Serial.println("loop");
 
-  delay(1000);
+  max_matrix.setDot(1,1, 1);
+
+  delay(100);
 }
