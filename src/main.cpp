17a43b17604bab4e6a19f84fee0930cc145721d1
Terry Wooton <terry@oodle.io>
2020-11-25 20:58:33 -0500

    max matrix

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,11 @@
 
 #include <Arduino.h>
 #include <Helpers.h>
+#include <IRremote.h>
 #include <MaxMatrix.h>
+
+IRrecv ir_receive(DIGITAL_IO_PIN(7));
+decode_results ir_receiver_results;
 
 const int MAX_MATRICIES_ON_BOARD = 1;
 const int MAX_ROWS = 8;
@@ -15,21 +19,28 @@ const int MAX_COLS = 8;
 const int MAX_MATRIX_DIN_PIN = DIGITAL_IO_PIN(11);
 const int MAX_MATRIX_CS_LOAD_PIN = DIGITAL_IO_PIN(10);
 const int MAX_MATRIX_CLK_PIN = DIGITAL_IO_PIN(13);
-
 MaxMatrix max_matrix(MAX_MATRIX_DIN_PIN, MAX_MATRIX_CS_LOAD_PIN, MAX_MATRIX_CLK_PIN, MAX_MATRICIES_ON_BOARD);
 
 void setup()
 {
   Serial.begin(9600);
+
+  ir_receive.enableIRIn();
+
   max_matrix.init();
   max_matrix.setIntensity(0);
 }
 
 void loop()
 {
-  Serial.println("loop");
+  Serial.println("Loop");
 
-  max_matrix.setDot(1,1, 1);
+  max_matrix.setDot(1, 1, 1);
 
-  delay(100);
+  if (ir_receive.decode(&ir_receiver_results)) {
+    Serial.println(ir_receiver_results.value, HEX);
+    ir_receive.resume();
+  }
+
+  delay(600);
 }
