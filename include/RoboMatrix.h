d308fd77cce374c0d16d32c3dbd8c0e93695d557
Terry Wooton <terry@oodle.io>
2020-11-27 09:18:53 -0500

    matrix refactor

---
+++ include/RoboMatrix.h
@@ -0,0 +1,88 @@
+#ifndef ROBO_MATRIX_H
+#define ROBO_MATRIX_H
+
+#include <Arduino.h>
+#include <MaxMatrix.h>
+#include <AbstractRoboPart.h>
+
+class RoboMatrix : AbstractRoboPart {
+    static const int DIM = 8;
+    const int m_din_pin;
+    const int m_cs_load_pin;
+    const int m_clk_pin;
+    const int m_matricies_on_board;
+    const int m_max_rows;
+    const int m_max_cols;
+
+    MaxMatrix m_max_matrix;
+    int current_matrix_x = 0;
+    int current_matrix_y = 0;
+
+    public:
+
+    RoboMatrix(RoboBrain& robo_brain, const int din_pin, const int cs_load_pin, const int clk_pin, const int matricies_on_board) :
+        AbstractRoboPart(robo_brain),
+        m_din_pin(din_pin),
+        m_cs_load_pin(cs_load_pin),
+        m_clk_pin(clk_pin),
+        m_matricies_on_board(matricies_on_board),
+        m_max_rows(DIM),
+        m_max_cols(DIM * matricies_on_board),
+        m_max_matrix(din_pin, cs_load_pin, clk_pin, matricies_on_board)
+    {
+        robo_brain.add_part(this);
+    }
+
+    inline void setup() override {
+        m_max_matrix.init();
+        m_max_matrix.setIntensity(0);
+    }
+
+
+    inline void clear_current() {
+        m_max_matrix.setDot(current_matrix_x, current_matrix_y, 0);
+    }
+
+    inline void move_x(const int movement)
+    {
+        clear_current();
+
+        int new_x = (current_matrix_x + movement);
+        if(new_x < 0) {
+            new_x = 0;
+        }
+        else if(new_x >= m_max_cols) {
+            new_x = m_max_cols-1;
+        }
+
+        current_matrix_x = new_x;
+
+        render();
+    }
+
+    inline void move_y(const int movement)
+    {
+        clear_current();
+
+        int new_y = (current_matrix_y + movement);
+        if(new_y < 0) {
+            new_y = 0;
+        }
+        else if(new_y >= m_max_cols) {
+            new_y = m_max_cols-1;
+        }
+
+        current_matrix_y = new_y;
+
+        render();
+    }
+
+    private:
+
+    inline void render()
+    {
+        m_max_matrix.setDot(current_matrix_x, current_matrix_y, 1);
+    }
+};
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -2,26 +2,19 @@
   Arduino Tetris
   Author: twooton
   Email: tcwooton@gmail.com
-  Description: Simple Robot
+  Description: Arduino Tetris
 */
 
 #include <Arduino.h>
 #include <Helpers.h>
-#include <MaxMatrix.h>
 #include <RoboBrain.h>
+#include <RoboMatrix.h>
 #include <RoboIRReceiver.h>
 
-const int MAX_MATRICIES_ON_BOARD = 2;
-const int MAX_ROWS = 8;
-const int MAX_COLS = 16;
-const int MAX_MATRIX_DIN_PIN = DIGITAL_IO_PIN(11);
-const int MAX_MATRIX_CS_LOAD_PIN = DIGITAL_IO_PIN(10);
-const int MAX_MATRIX_CLK_PIN = DIGITAL_IO_PIN(13);
-MaxMatrix max_matrix(MAX_MATRIX_DIN_PIN, MAX_MATRIX_CS_LOAD_PIN, MAX_MATRIX_CLK_PIN, MAX_MATRICIES_ON_BOARD);
-int current_matrix_x = 0;
-int current_matrix_y = 0;
-
 RoboBrain robo_brain;
+
+const int max_matricies_on_board = 2;
+RoboMatrix robo_matrix(robo_brain, DIGITAL_IO_PIN(11), DIGITAL_IO_PIN(10), DIGITAL_IO_PIN(13), max_matricies_on_board);
 
 RoboIRReceiver robo_ir_receiver(robo_brain, DIGITAL_IO_PIN(7));
 
@@ -30,83 +23,34 @@ void setup()
   Serial.begin(9600);
 
   robo_brain.setup();
-
-  max_matrix.init();
-  max_matrix.setIntensity(0);
-}
-
-inline void clear_current() {
-  max_matrix.setDot(current_matrix_x, current_matrix_y, 0);
-}
-
-inline void move_x(const int movement)
-{
-  clear_current();
-
-  int new_x = (current_matrix_x + movement);
-  if(new_x < 0) {
-    new_x = 0;
-  }
-  else if(new_x >= MAX_COLS ) {
-    new_x = MAX_COLS -1;
-  }
-
-  current_matrix_x = new_x;
-}
-
-inline void move_y(const int movement)
-{
-  clear_current();
-
-  int new_y = (current_matrix_y + movement);
-  if(new_y < 0) {
-    new_y = 0;
-  }
-  else if(new_y >= MAX_COLS ) {
-    new_y = MAX_COLS -1;
-  }
-
-  current_matrix_y = new_y;
 }
 
 void loop()
 {
-  /*
-  Serial.print("x: ");
-  Serial.print(current_matrix_x);
-  Serial.print(" y: ");
-  Serial.println(current_matrix_y);
-  */
-
   if (robo_ir_receiver.detect_signal()) {
     const RoboIRReceiver::IRCode ir_code = robo_ir_receiver.get_ir_code();
     Serial.print("IR code: ");
     Serial.println(ir_code);
 
-    // switch(ir_code_to_consider) {
-    //   case IR_CODE_LEFT:
-    //     // move_x(-1);
-    //     break;
-    //   case IR_CODE_RIGHT:
-    //     // move_x(1);
-    //     break;
-    //   case IR_CODE_UP:
-    //     // move_y(-1);
-    //     break;
-    //   case IR_CODE_DOWN:
-    //     // move_y(1);
-    //     break;
-    //   default:
-    //     // move_x(1);
-    //     break;
-    // }
+    switch(ir_code) {
+      case RoboIRReceiver::IR_CODE_LEFT:
+        robo_matrix.move_x(-1);
+        break;
+      case RoboIRReceiver::IR_CODE_RIGHT:
+        robo_matrix.move_x(1);
+        break;
+      case RoboIRReceiver::IR_CODE_UP:
+        robo_matrix.move_y(-1);
+        break;
+      case RoboIRReceiver::IR_CODE_DOWN:
+        robo_matrix.move_y(1);
+        break;
+      default:
+        robo_matrix.move_x(1);
+        break;
+    }
     robo_ir_receiver.resume();
   }
-  else {
-    // move_x(-1);
-  }
-
-  //max_matrix.setDot(current_matrix_x, current_matrix_y, 1);
 
   delay(500);
 }
