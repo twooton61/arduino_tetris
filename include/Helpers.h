d9cf875c92eb7f9341e0ab44a951e7f1263865cb
Terry Wooton <terry@oodle.io>
2020-12-10 08:08:37 -0500

    fix GET helper

--- include/Helpers.h
+++ include/Helpers.h
@@ -6,7 +6,7 @@
 #define DIGITAL_IO_PIN(pin) pin
 
 #define DISABLE_COPY_AND_ASSIGN(Class) Class(const Class& x) = delete; Class& operator=(const Class x) = delete;
-#define GET(type, var) public: const type var() { return m_##var; }
+#define GET(type, var) type var() const { return m_##var; }
 #define SET(type, var) void var(const type var) { m_##var = var; }
 #define GET_SET(type, var) GET(type, var) SET(type, var)
 
